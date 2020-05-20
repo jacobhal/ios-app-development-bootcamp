2a48872c78fba0dfb3fe7ba833b6c1e3c5f898b0
Jacob <jackeaik@hotmail.com>
2020-05-20 22:20:49 +0200

    I Am Rich without constraints complete

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jacob Hallman on 2020-05-19.
+//  Copyright © 2020 Jacob Hallman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
