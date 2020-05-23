5fc6d8b6d7f65f8f6c3130d5354932b25fbfb0b6
Jacob <jackeaik@hotmail.com>
2020-05-23 13:47:30 +0200

    Xylophone project + boss challenge completed

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        // Reduce opacity to half (set the background opacity if we do not want to affect the label)
+        sender.backgroundColor = sender.backgroundColor?.withAlphaComponent(0.5)
+        // sender.alpha = 0.5
+        playSound(soundName: sender.currentTitle!)
+        //        playSound(soundName: sender.titleLabel?.text)
+        //        playSound(soundName: sender.title(for: .normal) ?? "")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //sender.alpha = 1
+            sender.backgroundColor = sender.backgroundColor?.withAlphaComponent(1)
+        }
+    }
+    
+    func playSound(soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print("playSound error: " + error.localizedDescription)
+        }
+    }
     
 
 }
